d383085c314300926f7c1268598cbcfa6e0c786d
md rafiqul islam <rafiqulislam11002@gmail.com>
2022-09-29 12:22:32 +0600

    footer added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-
+            <div className="containter pt-5 pb-2">
+                <p>Md Rafiqul Islam Creation. All reserve rights</p>
+                Ⓒ 20202
+            </div>
         </div>
     );
 };
