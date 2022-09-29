c8e5d18884fe74fe080f6e7c0bbd7215a2546072
md rafiqul islam <rafiqulislam11002@gmail.com>
2022-09-29 17:18:27 +0600

    animation deleted due to mobile responsive issue

--- src/App.js
+++ src/App.js
@@ -3,16 +3,10 @@ import Home from './components/Home/Home';
 import Header from './components/Header/Header';
 import Question from './components/Question/Question';
 import Footer from './components/Footer/Footer';
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { useEffect } from 'react';
+
 
 
 function App() {
-  useEffect(() => {
-    AOS.init();
-    AOS.refresh();
-  }, []);
   return (
     <div className="App">
       <Header></Header>
