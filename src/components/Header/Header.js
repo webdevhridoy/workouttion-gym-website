9f545f6405208ab978878cc28a0a779be5a522cd
md rafiqul islam <rafiqulislam11002@gmail.com>
2022-09-29 21:29:44 +0600

    text responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div className='header-logo container text-start'>
+        <div className='header-logo container text-center text-md-start'>
             <img src={logo} className="App-logo" alt="logo" />
 
         </div>
